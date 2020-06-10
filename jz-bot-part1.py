b6780214b14e9ac9af11a6ab8eb8740681edc2f7
SIPtopia <64345845+SIPtopia@users.noreply.github.com>
2020-06-10 14:55:19 -0500

    Add files via upload

---
+++ jz-bot-part1.py
@@ -0,0 +1,84 @@
+from selenium import webdriver
+from time import sleep
+
+class SimpleBot:
+
+    def addingmytext(self):
+        url = "<Your target URL goes here>"
+        mybrowser = webdriver.Chrome(executable_path="<Full path to the Chrome driver you downloaded>")
+        mybrowser.get(url)
+        mybrowser.maximize_window()
+
+        # sleep(3)
+        # element1 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element1.click()
+        # OR
+        # element1.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+        # sleep(3)
+        # element2 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element2.click()
+        # OR
+        # element2.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+        # sleep(3)
+        # element3 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element3.click()
+        # OR
+        # element3.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+        # sleep(3)
+        # element4 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element4.click()
+        # OR
+        # element4.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+        # sleep(3)
+        # element5 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element5.click()
+        # OR
+        # element5.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+        # sleep(3)
+        # element6 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element6.click()
+        # OR
+        # element6.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+        # sleep(3)
+        # element7 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element7.click()
+        # OR
+        # element7.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+        # sleep(3)
+        # element8 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element8.click()
+        # OR
+        # element8.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+        # sleep(3)
+        # element9 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element9.click()
+        # OR
+        # element9.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+        # sleep(3)
+        # element10 = mybrowser.find_element_by_xpath("<Element's XPATH goes here>")
+        # element10.click()
+        # OR
+        # element10.send_keys("<whatever text you want to enter goes here>")
+        # sleep(3)
+
+Iam = SimpleBot().addingmytext()
+
+
